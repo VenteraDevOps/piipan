34960453b7ad2950404d345b4a3457f5cdcd5372
Dan Borgen <dborgen@VENTERA.com>
2022-03-25 10:40:40 -0500

    Adding utilities to query tool

---
+++ query-tool/src/Piipan.QueryTool/wwwroot/js/utilities.js
@@ -0,0 +1,35 @@
+﻿(function piipanUtilities() {
+    let reference;
+    if (!window.piipan) {
+        window.piipan = {};
+    }
+    if (!window.piipan.utilities) {
+        window.piipan.utilities = {};
+    }
+    window.piipan.utilities.getCursorPosition = (element) => {
+        return element.selectionStart;
+    }
+    window.piipan.utilities.setCursorPosition = (element, position) => {
+        element.setSelectionRange(position, position);
+    }
+    window.piipan.utilities.setValue = (element, value) => {
+        element.value = value;
+    }
+    window.piipan.utilities.focusElement = (id) => {
+        document.getElementById(id)?.focus();
+    }
+    window.piipan.utilities.doesElementHaveInvalidInput = (id) => {
+        return document.getElementById(id)?.validity?.badInput || false;
+    }
+    window.piipan.utilities.validateForm = async (event) => {
+        const valid = await event.target.dotNetReference.invokeMethodAsync('ValidateForm');
+        if (valid) {
+            event.target.submit();
+        }
+    }
+    window.piipan.utilities.registerFormValidation = (formId, dotNetReference) => {
+        const form = document.getElementById(formId);
+        form.dotNetReference = dotNetReference;
+        form.addEventListener('submit', window.piipan.utilities.validateForm);
+    }
+}());
