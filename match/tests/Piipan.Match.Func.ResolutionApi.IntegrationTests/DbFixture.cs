a2fc28703d527c0f982cda37ff9996ae9183648b
Eleni Chappen <eleni.chappen@gsa.gov>
2022-03-25 15:44:19 -0500

    removing core integration test dep (#2895)

---
+++ match/tests/Piipan.Match.Func.ResolutionApi.IntegrationTests/DbFixture.cs
@@ -0,0 +1,282 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Dapper;
+using Npgsql;
+using Piipan.Match.Api.Models;
+using Piipan.Match.Core.Models;
+using Piipan.Match.Core.DataAccessObjects;
+
+namespace Piipan.Match.Core.IntegrationTests
+{
+    /// <summary>
+    /// Test fixture for match records integration testing.
+    /// Creates a fresh matches tables, dropping it when testing is complete.
+    /// </summary>
+    public class DbFixture : IDisposable
+    {
+        public readonly string ConnectionString;
+        public readonly NpgsqlFactory Factory;
+
+        public DbFixture()
+        {
+            ConnectionString = Environment.GetEnvironmentVariable("CollaborationDatabaseConnectionString");
+            Factory = NpgsqlFactory.Instance;
+
+            Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
+
+            Initialize();
+            ApplySchema();
+        }
+
+        /// <summary>
+        /// Ensure the database is able to receive connections before proceeding.
+        /// </summary>
+        public void Initialize()
+        {
+            var retries = 10;
+            var wait = 2000; // ms
+
+            while (retries >= 0)
+            {
+                try
+                {
+                    using (var conn = Factory.CreateConnection())
+                    {
+                        conn.ConnectionString = ConnectionString;
+                        conn.Open();
+                        conn.Close();
+
+                        return;
+                    }
+                }
+                catch (Npgsql.NpgsqlException ex)
+                {
+                    retries--;
+                    Console.WriteLine(ex.Message);
+                    Thread.Sleep(wait);
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            using (var conn = Factory.CreateConnection())
+            {
+                conn.ConnectionString = ConnectionString;
+                conn.Open();
+
+                conn.Execute("DROP INDEX IF EXISTS index_match_id_on_match_res_events");
+                conn.Execute("DROP TABLE IF EXISTS match_res_events");
+                conn.Execute("DROP TABLE IF EXISTS matches");
+                conn.Execute("DROP TYPE IF EXISTS hash_type");
+
+                conn.Close();
+            }
+
+        }
+
+        private void ApplySchema()
+        {
+            string sqltext = System.IO.File.ReadAllText("match-record.sql", System.Text.Encoding.UTF8);
+
+            using (var conn = Factory.CreateConnection())
+            {
+                conn.ConnectionString = ConnectionString;
+                conn.Open();
+
+                conn.Execute("DROP INDEX IF EXISTS index_match_id_on_match_res_events");
+                conn.Execute("DROP TABLE IF EXISTS match_res_events");
+                conn.Execute("DROP TABLE IF EXISTS matches");
+                conn.Execute("DROP TYPE IF EXISTS hash_type");
+                conn.Execute(sqltext);
+
+                conn.Close();
+            }
+        }
+
+        public void ClearMatchRecords()
+        {
+            using (var conn = Factory.CreateConnection())
+            {
+                conn.ConnectionString = ConnectionString;
+                conn.Open();
+
+                conn.Execute("DELETE FROM matches");
+
+                conn.Close();
+            }
+        }
+
+        public void ClearMatchResEvents()
+        {
+            using (var conn = Factory.CreateConnection())
+            {
+                conn.ConnectionString = ConnectionString;
+                conn.Open();
+
+                conn.Execute("DELETE FROM match_res_events");
+
+                conn.Close();
+            }
+        }
+
+        public void Insert(MatchRecordDbo record)
+        {
+            var factory = NpgsqlFactory.Instance;
+
+            using (var conn = factory.CreateConnection())
+            {
+                conn.ConnectionString = ConnectionString;
+                conn.Open();
+
+                conn.Execute(@"
+                    INSERT INTO matches
+                    (
+                        created_at,
+                        match_id,
+                        initiator,
+                        states,
+                        hash,
+                        hash_type,
+                        input,
+                        data
+                    )
+                    VALUES
+                    (
+                        now() at time zone 'utc',
+                        @MatchId,
+                        @Initiator,
+                        @States,
+                        @Hash,
+                        @HashType::hash_type,
+                        @Input::jsonb,
+                        @Data::jsonb
+                    )", record);
+
+                conn.Close();
+            }
+        }
+
+        public void InsertMatchResEvent(MatchResEventDbo record)
+        {
+            var factory = NpgsqlFactory.Instance;
+
+            using (var conn = factory.CreateConnection())
+            {
+                conn.ConnectionString = ConnectionString;
+                conn.Open();
+
+                conn.Execute(@"
+                    INSERT INTO match_res_events
+                    (
+                        match_id,
+                        actor,
+                        actor_state,
+                        delta
+                    )
+                    VALUES
+                    (
+                        @MatchId,
+                        @Actor,
+                        @ActorState,
+                        @Delta::jsonb
+                    )", record);
+
+                conn.Close();
+            }
+        }
+
+        public bool HasRecord(MatchRecordDbo record)
+        {
+            var result = false;
+            var factory = NpgsqlFactory.Instance;
+
+            using (var conn = factory.CreateConnection())
+            {
+                conn.ConnectionString = ConnectionString;
+                conn.Open();
+
+                var row = conn.QuerySingle<MatchRecordDbo>(@"
+                    SELECT match_id,
+                        created_at,
+                        initiator,
+                        hash,
+                        hash_type::text,
+                        states,
+                        input,
+                        data
+                    FROM matches
+                    WHERE match_id=@MatchId", record);
+
+                result = row.Equals(record);
+
+                conn.Close();
+            }
+
+            return result;
+        }
+
+        public bool HasMatchResEvent(MatchResEventDbo record)
+        {
+            var result = false;
+            var factory = NpgsqlFactory.Instance;
+
+            using (var conn = factory.CreateConnection())
+            {
+                conn.ConnectionString = ConnectionString;
+                conn.Open();
+
+                var row = conn.QuerySingle<MatchResEventDbo>(@"
+                    SELECT
+                        id,
+                        inserted_at,
+                        match_id,
+                        actor,
+                        actor_state,
+                        delta
+                    FROM match_res_events
+                    WHERE id=@Id
+                    ", record);
+
+                result = row.Id.Equals(record.Id);
+
+                conn.Close();
+            }
+
+            return result;
+        }
+
+        public async Task<IEnumerable<IMatchResEvent>> GetEvents(string matchId)
+        {
+            const string sql = @"
+                SELECT
+                    id,
+                    match_id,
+                    inserted_at,
+                    actor,
+                    actor_state,
+                    delta::jsonb
+                FROM match_res_events
+                WHERE
+                    match_id=@MatchId
+                ORDER BY inserted_at asc
+                ;";
+            var parameters = new {
+                MatchId = matchId,
+            };
+            var factory = NpgsqlFactory.Instance;
+            IEnumerable<MatchResEventDbo> result;
+
+            using (var conn = factory.CreateConnection())
+            {
+                conn.ConnectionString = ConnectionString;
+                conn.Open();
+                result = await conn.QueryAsync<MatchResEventDbo>(sql, parameters);
+                conn.Close();
+            }
+            return result;
+        }
+    }
+}
