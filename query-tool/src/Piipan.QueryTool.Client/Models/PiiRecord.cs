87480d3d25ff33d979dee521d271ec695955fad8
Dan Borgen <dborgen@VENTERA.com>
2022-03-31 15:18:18 -0500

    Cleaning some files up

--- query-tool/src/Piipan.QueryTool.Client/Models/PiiRecord.cs
+++ query-tool/src/Piipan.QueryTool.Client/Models/PiiRecord.cs
@@ -1,5 +1,6 @@
 ﻿using Piipan.Components.Validation;
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace Piipan.QueryTool.Client.Models
 {
@@ -11,6 +12,7 @@ namespace Piipan.QueryTool.Client.Models
         [UsaRequired]
         [UsaName]
         [Display(Name = "Last Name")]
+        [JsonPropertyName("last")]
         public string? LastName { get; set; }
 
         [UsaRequired]
@@ -18,17 +20,21 @@ namespace Piipan.QueryTool.Client.Models
         [DataType(DataType.Date),
             DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
         [DateOfBirthRange("01/01/1900", ErrorMessage = "@@@ must be between 01-01-1900 and today's date")]
+        [JsonPropertyName("dob")]
         public DateTime? DateOfBirth { get; set; }
 
         [UsaRequired]
         [UsaSSN]
         [Display(Name = "Social Security Number")]
+        [JsonPropertyName("ssn")]
         public string? SocialSecurityNum { get; set; }
 
         [Display(Name = "Participant ID")]
+        [JsonPropertyName("participant_id")]
         public string? ParticipantId { get; set; }
 
         [Display(Name = "Case Number")]
+        [JsonPropertyName("case_id")]
         public string? CaseId { get; set; }
     }
 }
