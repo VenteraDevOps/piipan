c4c399c83eeb9348300635e55997b4a86abe24c0
Bill Foshay <bfoshay@VENTERA.com>
2022-04-01 15:59:46 -0400

    Reverting CustomerEncryptedBlobRetrievalService

--- etl/src/Piipan.Etl/Piipan.Etl.Func.BulkUpload/Services/CustomerEncryptedBlobRetrievalService.cs
+++ etl/src/Piipan.Etl/Piipan.Etl.Func.BulkUpload/Services/CustomerEncryptedBlobRetrievalService.cs
@@ -9,16 +9,11 @@ namespace Piipan.Etl.Func.BulkUpload.Services
         public BlobClient RetrieveBlob(string storageAccountConnectionString, string blobName, string customerProvidedKey)
         {
             BlobClientOptions blobClientOptions = new BlobClientOptions() { CustomerProvidedKey = new Azure.Storage.Blobs.Models.CustomerProvidedKey(customerProvidedKey) };
-            BlobServiceClient blobServiceClient = RetrieveBlobServiceClient(storageAccountConnectionString, blobClientOptions);
+            BlobServiceClient blobServiceClient = new BlobServiceClient(storageAccountConnectionString, blobClientOptions);
             
             BlobContainerClient blobContainerClient = blobServiceClient.GetBlobContainerClient(UPLOAD_CONTAINER_NAME);
             BlobClient blobClient = blobContainerClient.GetBlobClient(blobName);
             return blobClient;
-        }
-
-        public BlobServiceClient RetrieveBlobServiceClient(string connectionString, BlobClientOptions options)
-        {
-            return new BlobServiceClient(connectionString, options);
         }
     }
 }
