7ba375ae5b5cafa85cbe88d910255ef53fa0e443
Bill Foshay <bfoshay@VENTERA.com>
2022-04-01 14:17:33 -0400

    Adding code to retrieve blob name from EventGridEvent

--- etl/src/Piipan.Etl/Piipan.Etl.Func.BulkUpload/Services/CustomerEncryptedBlobRetrievalService.cs
+++ etl/src/Piipan.Etl/Piipan.Etl.Func.BulkUpload/Services/CustomerEncryptedBlobRetrievalService.cs
@@ -4,12 +4,14 @@ namespace Piipan.Etl.Func.BulkUpload.Services
 {
     public class CustomerEncryptedBlobRetrievalService : ICustomerEncryptedBlobRetrievalService
     {
+        private const string UPLOAD_CONTAINER_NAME = "upload";
+
         public BlobClient RetrieveBlob(string storageAccountConnectionString, string blobName, string customerProvidedKey)
         {
             BlobClientOptions blobClientOptions = new BlobClientOptions() { CustomerProvidedKey = new Azure.Storage.Blobs.Models.CustomerProvidedKey(customerProvidedKey) };
             BlobServiceClient blobServiceClient = new BlobServiceClient(storageAccountConnectionString, blobClientOptions);
-
-            BlobContainerClient blobContainerClient = blobServiceClient.GetBlobContainerClient("upload");
+            
+            BlobContainerClient blobContainerClient = blobServiceClient.GetBlobContainerClient(UPLOAD_CONTAINER_NAME);
             BlobClient blobClient = blobContainerClient.GetBlobClient(blobName);
             return blobClient;
         }
