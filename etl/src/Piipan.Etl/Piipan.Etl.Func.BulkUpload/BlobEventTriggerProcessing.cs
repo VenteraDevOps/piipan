e18ae9f09e1a410855dc3cb4d0b11d97ac74ecab
Edwin Torres <edwin.torres@gsa.gov>
2022-04-13 13:46:01 -0400

    Renamed bulkupload and QueueBulkUpload

---
+++ etl/src/Piipan.Etl/Piipan.Etl.Func.BulkUpload/BlobEventTriggerProcessing.cs
@@ -0,0 +1,73 @@
+// Default URL for triggering event grid function in the local environment.
+// http://localhost:7071/runtime/webhooks/EventGrid?functionName={functionname}
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Azure.EventGrid.Models;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.EventGrid;
+using Microsoft.Extensions.Logging;
+using Piipan.Etl.Func.BulkUpload.Parsers;
+using Piipan.Participants.Api;
+using Azure.Storage.Blobs;
+
+namespace Piipan.Etl.Func.BulkUpload
+{
+    /// <summary>
+    /// Azure Function implementing basic Extract-Transform-Load of piipan
+    /// bulk import put the CSV file names, that where imported in the Storage Container, in a Queue Storage.
+    /// </summary>
+    public class BlobEventTriggerProcessing
+    {
+        private readonly IParticipantApi _participantApi;
+        private readonly IParticipantStreamParser _participantParser;
+
+        public BlobEventTriggerProcessing(
+            IParticipantApi participantApi,
+            IParticipantStreamParser participantParser)
+        {
+            _participantApi = participantApi;
+            _participantParser = participantParser;
+        }
+
+        /// <summary>
+        /// Entry point for the state-specific Azure Function instance
+        /// </summary>
+        /// <param name="eventGridEvent">storage container blob creation event</param>
+        /// <param name="input">handle to CSV file uploaded to a state-specific container</param>
+        /// <param name="log">handle to the function log</param>
+        /// <remarks>
+        /// The function is expected to be executing as a managed identity that has read access
+        /// to the per-state storage account and write access to the per-state database.
+        /// </remarks>
+        [FunctionName("BlobEventTriggerProcessing")]
+        [return: Queue("qupload", Connection = "BlobStorageConnectionString")]
+        public string Run(
+            [EventGridTrigger] EventGridEvent eventGridEvent,
+            [Blob("{data.url}", FileAccess.Read, Connection = "BlobStorageConnectionString")] BlobClient blob,
+            ILogger log)
+        {
+            try
+            {
+                if (blob != null)
+                {
+                    log.LogInformation(eventGridEvent.Data.ToString());
+                    log.LogInformation($"{blob.Name}");
+
+                    return blob.Name;
+                }
+                else
+                {
+                    log.LogError("No input stream was provided");
+                    return "";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                log.LogError(ex.Message);
+                throw;
+            }
+        }
+    }
+}
