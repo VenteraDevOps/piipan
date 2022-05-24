3e423a96f5e36f4aa899a2e4c4ff41179b8564d8
Conner Caprio <ccaprio@VENTERA.com>
2022-05-24 14:57:05 -0400

    adding all needed changes

--- Piipan.Dashboard.Client/Program.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Piipan.Dashboard.Client;
-
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-await builder.Build().RunAsync();
