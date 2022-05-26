c8757952dab5ffd4182ff58d05d8ecdd04cf00f0
Conner Caprio <ccaprio@VENTERA.com>
2022-05-26 17:08:24 -0400

    removed un needed variables

--- dashboard/src/Piipan.Dashboard.Client/Models/ParticipantUploadModel.cs
+++ dashboard/src/Piipan.Dashboard.Client/Models/ParticipantUploadModel.cs
@@ -5,11 +5,5 @@ namespace Piipan.Dashboard.Client.Models
     public class ParticipantUploadModel
     {
         public List<ParticipantUpload> ParticipantUploadResults { get; set; } = new List<ParticipantUpload>();
-
-        //optional? below
-        public string? NextPageParams { get; private set; }
-        public string? PrevPageParams { get; private set; }
-        public string? StateQuery { get; private set; }
-        public static int PerPageDefault = 10;
     }
 }
