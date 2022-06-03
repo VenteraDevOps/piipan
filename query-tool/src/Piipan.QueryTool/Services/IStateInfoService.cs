3e88f4f52c6a4d883bcd94db7891f6d89f4fbf54
Dan Borgen <dborgen@VENTERA.com>
2022-06-03 09:36:45 -0500

    State Information service

---
+++ query-tool/src/Piipan.QueryTool/Services/IStateInfoService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Piipan.Match.Api.Models;
+
+namespace Piipan.QueryTool.Services
+{
+    public interface IStateInfoService
+    {
+        public Task<StateInfoResponse> GetStateInfoAsync();
+    }
+}
