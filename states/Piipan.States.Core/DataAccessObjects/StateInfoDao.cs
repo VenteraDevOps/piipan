360f5f9cda50f4ab3cdb637013c1b2b8060afaa8
Conner Caprio <ccaprio@VENTERA.com>
2022-06-06 14:36:11 -0400

    init commit

---
+++ states/Piipan.States.Core/DataAccessObjects/StateInfoDao.cs
@@ -0,0 +1,48 @@
+﻿using Piipan.States.Api.Models;
+using Piipan.Shared.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Piipan.States.Core.Models;
+using Dapper;
+
+namespace Piipan.States.Core.DataAccessObjects
+{
+    public class StateInfoDao : IStateInfoDao
+    {
+        private readonly IDbConnectionFactory<StateInfoDb> _dbConnectionFactory;
+
+        public StateInfoDao(IDbConnectionFactory<StateInfoDb> dbConnectionFactory)
+        {
+            _dbConnectionFactory = dbConnectionFactory;
+        }
+
+        public async Task<IState> GetStateById(string id)
+        {
+            using (var connection = await _dbConnectionFactory.Build())
+            {
+                return await connection
+                    .QuerySingleAsync<StateInfoDbo>(@"
+                    SELECT id, state, state_abbreviation, email, phone, region
+	                FROM state_info WHERE id =@id;
+                    ORDER BY id DESC
+                    LIMIT 1", new { id = id });
+            }
+        }
+
+        public async Task<IState> GetStateByName(string state)
+        {
+            using (var connection = await _dbConnectionFactory.Build())
+            {
+                return await connection
+                    .QuerySingleAsync<StateInfoDbo>(@"
+                    SELECT id, state, state_abbreviation, email, phone, region
+	                FROM state_info WHERE state =@state;
+                    ORDER BY id DESC
+                    LIMIT 1", new { state = state });
+            }
+        }
+    }
+}
---
+++ states/Piipan.States.Core/Models/StateInfoDbo.cs
@@ -0,0 +1,19 @@
+﻿using Piipan.States.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Piipan.States.Core.Models
+{
+    public class StateInfoDbo : IState
+    {
+        public string? Id { get; set; }
+        public string State { get; set; }
+        public string StateAbbreviation { get; set; }
+        public string Email { get; set; }
+        public string? Phone { get; set; }
+        public string Region { get; set; }
+    }
+}
