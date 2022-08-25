68e6dda7cd3dccacf2468267b6b654b00f7a62ee
Foshay <12001004386445@FEDIDCARD.GOV>
2022-08-25 11:03:10 -0400

    Fix python script calls in configure-encryption-secrets

--- iac/configure-encryption-secrets.bash
+++ iac/configure-encryption-secrets.bash
@@ -53,14 +53,14 @@ main () {
 
     echo "Generate Column Encryption Key"
     columnKey=$(openssl enc -aes-256-cbc -k secret -P -md sha512 -pbkdf2 | sed -n 's/^key=\(.*\)/\1/p')
-    BASE64COLUMNKEY=$(../tools/hexToBase64Converter.py "${columnKey}")
+    BASE64COLUMNKEY=$(python ../tools/hexToBase64Converter.py "${columnKey}")
     create_kv_secret "${COLUMN_ENCRYPT_KEY_KV}" "${BASE64COLUMNKEY}"
 
     echo "Generate Payload Encryption Keys"
     payloadKey=$(openssl enc -aes-256-cbc -k secret -P -md sha512 -pbkdf2 | sed -n 's/^key=\(.*\)/\1/p')
-    BASE64UPLOADKEY=$(../tools/hexToBase64Converter.py "${payloadKey}")
-    shaOfPayloadKey=$(../tools/sha256Calculator.py "${payloadKey}")
-    BASE64UPLOADSHAKEY=$(../tools/hexToBase64Converter.py "${shaOfPayloadKey}")
+    BASE64UPLOADKEY=$(python ../tools/hexToBase64Converter.py "${payloadKey}")
+    shaOfPayloadKey=$(python ../tools/sha256Calculator.py "${payloadKey}")
+    BASE64UPLOADSHAKEY=$(python ../tools/hexToBase64Converter.py "${shaOfPayloadKey}")
     create_kv_secret "${UPLOAD_ENCRYPT_KEY_KV}" "${BASE64UPLOADKEY}"
     create_kv_secret "${UPLOAD_ENCRYPT_KEY_SHA_KV}" "${BASE64UPLOADSHAKEY}"
 }
