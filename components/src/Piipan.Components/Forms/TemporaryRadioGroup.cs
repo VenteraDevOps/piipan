c5ab297a2d42ef0114dc5a0b62d44dd6b07adc74
Dan Borgen <dborgen@VENTERA.com>
2022-09-08 10:45:38 -0500

    Fixes for radio buttons

---
+++ components/src/Piipan.Components/Forms/TemporaryRadioGroup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace Piipan.Components.Forms
+{
+    public class TemporaryRadioGroup<TValue> : InputRadioGroup<TValue>
+    {
+        private string? _name;
+
+        protected override void OnParametersSet()
+        {
+            if (Name != _name)
+            {
+                _name = Name;
+                base.OnParametersSet();
+            }
+        }
+    }
+}
